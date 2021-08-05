a513d879254e18c67fdf0cabeb91832b500d2430
renontech <pipipinon2552@gmail.com>
2021-08-05 15:55:40 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(strong)
+    if @comment.save
+      redirect_to prototype_path(@comment.prototype)
+    else
+      @prototype = Prototype.find(params[:prototype_id])
+      render "prototypes/show"
+    end
+  end
+
+  private
+  def strong
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
